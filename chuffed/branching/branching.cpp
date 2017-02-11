881b6e6598fa4769d0842d767ed86a184320b01d
Adrian Goldwaser <agold@hotmail.com>
2017-02-11 22:52:50 +1100

    Add prioritised branching group

--- chuffed/branching/branching.cpp
+++ chuffed/branching/branching.cpp
@@ -72,16 +72,78 @@ DecInfo* BranchGroup::branch() {
 }
 
 
-void branch(vec<Branching*> x, VarBranch var_branch, ValBranch val_branch) {
-	engine.branching->add(new BranchGroup(x, var_branch, true));
-	if (val_branch == VAL_DEFAULT) return;
-	PreferredVal p;
-	switch (val_branch) {
-		case VAL_MIN: p = PV_MIN; break;
-		case VAL_MAX: p = PV_MAX; break;
-		case VAL_SPLIT_MIN: p = PV_SPLIT_MIN; break;
-		case VAL_SPLIT_MAX: p = PV_SPLIT_MAX; break;
-		default: CHUFFED_ERROR("The value selection branching is not yet supported\n");
+BranchGroup* branch(vec<Branching*> x, VarBranch var_branch, ValBranch val_branch) {
+	// branching->add(new BranchGroup(x, var_branch, true));
+	if (val_branch != VAL_DEFAULT) {
+        PreferredVal p;
+        switch (val_branch) {
+            case VAL_MIN: p = PV_MIN; break;
+            case VAL_MAX: p = PV_MAX; break;
+            case VAL_SPLIT_MIN: p = PV_SPLIT_MIN; break;
+            case VAL_SPLIT_MAX: p = PV_SPLIT_MAX; break;
+            default: CHUFFED_ERROR("The value selection branching is not yet supported\n");
+        }
+        for (int i = 0; i < x.size(); i++) ((Var*) x[i])->setPreferredVal(p);
+    }
+	return new BranchGroup(x, var_branch, true);
+}
+
+
+PriorityBranchGroup::PriorityBranchGroup(vec<Branching*>& x, VarBranch vb) :
+	decisionVars(x), var_branch(vb), fin(0), cur(-1) {}
+
+bool PriorityBranchGroup::finished() {
+	if (fin) return true;
+	for (int i = 0; i < annotations.size(); i++) {
+		if (!annotations[i]->finished()) return false;
 	}
-	for (int i = 0; i < x.size(); i++) ((Var*) x[i])->setPreferredVal(p);
+	fin = 1;
+	return true;
+}
+
+double PriorityBranchGroup::getScore(VarBranch vb) {
+	double sum = 0;
+	for (int i = 0; i < decisionVars.size(); i++) {
+		sum += decisionVars[i]->getScore(vb);
+	}
+	return sum / decisionVars.size();
+}
+
+DecInfo* PriorityBranchGroup::branch() {
+	// If we're working on a group and it's not finished, continue there
+	if (cur >= 0 && !annotations[cur]->finished()) return annotations[cur]->branch();
+
+
+	// Need to find new group
+	if (var_branch == VAR_INORDER) {
+		int i = 0;
+		while (i < annotations.size() && annotations[i]->finished()) i++;
+		if (i == annotations.size()) {
+			return NULL;
+		}
+		if (!terminal) cur = i;
+		return annotations[i]->branch();
+	}
+
+	double best = -1e100;
+	moves.clear();
+	for (int i = 0; i < annotations.size(); i++) {
+		if (annotations[i]->finished()) continue;
+		double s = decisionVars[i]->getScore(var_branch);
+		if (s >= best) {
+			if (s > best) {
+				best = s;
+				moves.clear();
+			}
+			moves.push(i);
+		}
+	}
+	if (moves.size() == 0) {
+		return NULL;
+	}
+	int best_i = moves[0];
+	if (so.branch_random) best_i = moves[rand()%moves.size()];
+
+	if (!terminal) cur = best_i;
+	return annotations[best_i]->branch();
 }
