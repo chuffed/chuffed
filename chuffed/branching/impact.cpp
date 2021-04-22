7224e4c08ef2143a5cc1e6f9be6384319ecc6307
Geremy Desmanche <lndi.powa@gmail.com>
2021-04-21 21:39:15 -0400

    Add resolution impact evaluation

--- chuffed/branching/impact.cpp
+++ chuffed/branching/impact.cpp
@@ -1,11 +1,11 @@
 #include <chuffed/branching/impact.h>
 
-constexpr double WEIGHT_LOCAL =  42;
-constexpr double WEIGHT_GLOBAL =  42;
+constexpr double WEIGHT_LOCAL = 420;
+constexpr double WEIGHT_GLOBAL = 420;
 constexpr double WEIGHT_REPARTITION =  42;
 constexpr double CLEARED_LOCAL = 0.42;
 constexpr double CLEARED_GLOBAL = 0.42;
-constexpr double CLEARED_REPARTITION = 0.42;
+constexpr double CLEARED_REPARTITION = 1.00;
 
 double processImpact(vec<int> const &previousSizes, vec<int> const &newSizes) {
 	int const n = newSizes.size();
@@ -25,6 +25,37 @@ double processImpact(vec<int> const &previousSizes, vec<int> const &newSizes) {
 			repartition += CLEARED_REPARTITION;
 		} else {
 			int const fi = ti - newSizes[i];
+			if (fi) {			
+				local += fi / ti;
+				global += fi;
+				++repartition;
+			}
+		}
+	}
+	return (  WEIGHT_LOCAL * local / n
+			+ WEIGHT_GLOBAL * global / total
+			+ WEIGHT_REPARTITION * repartition / n
+		) / (WEIGHT_LOCAL + WEIGHT_GLOBAL + WEIGHT_REPARTITION);
+}
+
+double solvedImpact(vec<int> const &previousSizes) {
+	int const n = previousSizes.size();
+	if (!n) NEVER;
+
+	double local = 0;
+	double global = 0;
+	int total = 0;
+	double repartition = 0;
+
+	for (int i = 0; i < n; ++i) {
+		int const ti = previousSizes[i];
+		total += ti;
+		if (previousSizes[i] == 1) {
+			local += CLEARED_LOCAL;
+			global += CLEARED_GLOBAL;
+			repartition += CLEARED_REPARTITION;
+		} else {
+			int const fi = ti - 1;
 			if (fi) {			
 				local += fi / ti;
 				global += fi;
