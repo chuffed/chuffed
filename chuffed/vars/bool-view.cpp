98f22aa2ccaddedf97f8fb4557fb0b7f0a222293
Jip J. Dekker <jip@dekker.one>
2023-05-30 14:43:23 +1000

    Allow attaching using EVENT_F to BoolView

--- chuffed/vars/bool-view.cpp
+++ chuffed/vars/bool-view.cpp
@@ -3,10 +3,10 @@
 
 void BoolView::attach(Propagator* p, int pos, int eflags) const {
 	WatchElem we(p->prop_id, pos);
-	if ((eflags & EVENT_L) != 0) {
+	if ((eflags & EVENT_L) != 0 || (eflags & EVENT_F) != 0) {
 		sat.watches[2 * v + static_cast<int>(s)].push(we);
 	}
-	if ((eflags & EVENT_U) != 0) {
+	if ((eflags & EVENT_U) != 0 || (eflags & EVENT_F) != 0) {
 		sat.watches[2 * v + (1 - static_cast<int>(s))].push(we);
 	}
 }
